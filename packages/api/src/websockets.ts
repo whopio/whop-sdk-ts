0e9209f522114b193b37a63c488c03d86e97e139
Jordan Jantschulev <j.jantschulev@gmail.com>
2025-05-16 09:47:25 -0400

    Improved code feng shui

--- packages/api/src/websockets.ts
+++ packages/api/src/websockets.ts
@@ -4,17 +4,3 @@ export type ReceivableWebsocketMessage =
 	proto.common.WebsocketMessageServerToClient;
 export type SendableWebsocketMessage =
 	proto.common.WebsocketMessageClientToServer;
-
-export function parseWebsocketMessage(
-	data: unknown,
-): ReceivableWebsocketMessage {
-	if (typeof data === "object" && data !== null) {
-		return data as ReceivableWebsocketMessage;
-	}
-
-	if (typeof data !== "string") {
-		throw new Error("Websocket message must be a string");
-	}
-
-	return JSON.parse(data);
-}
