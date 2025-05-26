6a1a7c11eb0718102f757b0d625dd7c625fd2996
Jordan Jantschulev <j.jantschulev@gmail.com>
2025-05-26 21:53:41 +0200

    feat: add client side broadcast to examples project

--- packages/api/src/index.shared.ts
+++ packages/api/src/index.shared.ts
@@ -9,10 +9,14 @@ export {
 	type VerifyUserTokenOptions,
 } from "@/verify-user-token";
 export { makeWebhookValidator, type WhopWebhookRequestBody } from "@/webhooks";
+export type { WhopWebsocketClientBrowser } from "@/websockets/client.browser";
 export type {
 	ReceivableWebsocketMessage,
 	SendableWebsocketMessage,
+	WebsocketBroadcastTarget,
 	WebsocketMessageHandler,
 	WebsocketStatus,
 	WebsocketStatusHandler,
+	WhopWebsocketClientBase,
 } from "@/websockets/client.common";
+export type { WhopWebsocketClientServer } from "@/websockets/client.server";
