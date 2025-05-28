26e0ca92636741d3660f766635576a76ff33804c
Jack Sharkey <sharkeyjack11@gmail.com>
2025-05-28 10:08:59 +0200

    Modify tutorial to have consistent naming

--- examples/ai-image-generator/app/api/experiences/[experienceId]/generate/route.ts
+++ examples/ai-image-generator/app/api/experiences/[experienceId]/generate/route.ts
@@ -112,7 +112,7 @@ export async function POST(
 		]);
 
 		const whopExperience = await whopApi.getExperience({ experienceId });
-		const bizId = whopExperience.experience.company.id;
+		const companyId = whopExperience.experience.company.id;
 
 		const generatedAttachmentId = uploadResponse.directUploadId;
 		const originalAttachmentId = originalFileUploadResponse.directUploadId;
@@ -126,7 +126,7 @@ export async function POST(
 		const post = await whopApi.createForumPost({
 			input: {
 				forumExperienceId: forumId,
-				content: `@${publicUser.publicUser?.username} generated this image with the prompt: "${experience.prompt}"\n\nTry it yourself here: https://whop.com/hub/${bizId}/${experience.id}/app\n\nBefore vs After ⬇️`,
+				content: `@${publicUser.publicUser?.username} generated this image with the prompt: "${experience.prompt}"\n\nTry it yourself here: https://whop.com/hub/${companyId}/${experience.id}/app\n\nBefore vs After ⬇️`,
 				attachments: [
 					{ directUploadId: originalAttachmentId },
 					{ directUploadId: generatedAttachmentId },
