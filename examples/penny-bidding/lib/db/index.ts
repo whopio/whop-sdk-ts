03b4a7496895204379f10f8876ddd4fbb739051f
Jordan Jantschulev <j.jantschulev@gmail.com>
2025-05-30 19:09:50 +0200

    vibe coding

--- examples/penny-bidding/lib/db/index.ts
+++ examples/penny-bidding/lib/db/index.ts
@@ -2,5 +2,9 @@ import { drizzle } from "drizzle-orm/postgres-js";
 
 import * as schema from "./schema";
 
-// biome-ignore lint/style/noNonNullAssertion: always set
-export const db = drizzle(process.env.POSTGRES_URL!, { schema });
+const DEFAULT_POSTGRES_URL =
+	"postgres://postgres:postgres@localhost:5432/postgres";
+
+export const db = drizzle(process.env.POSTGRES_URL ?? DEFAULT_POSTGRES_URL, {
+	schema,
+});
