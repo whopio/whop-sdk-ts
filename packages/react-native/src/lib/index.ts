feb2f14dbd1fe814b61dd5f41fb1a0b1770169c6
Jordan Jantschulev <j.jantschulev@gmail.com>
2025-07-17 16:07:26 -0400

    feat: Allow interacting with the internal native whop core api

--- packages/react-native/src/lib/index.ts
+++ packages/react-native/src/lib/index.ts
@@ -1,2 +1,3 @@
 export * from "./client-sdk";
+export * from "./native-whop-core-wrapper";
 export * from "./props";
---
+++ packages/react-native/src/lib/native-whop-core-wrapper.ts
@@ -0,0 +1,40 @@
+import nativeWhopCore from "./native-whop-core";
+
+// biome-ignore lint/complexity/noBannedTypes: allow here
+type EmptyObject = {};
+
+export interface ExecSyncApi {
+	getAppApiOrigin(params: EmptyObject): { apiOrigin: string };
+	cacheGet(params: { key?: string | null }): { data?: string | null };
+	cacheSet(params: { key?: string | null; data?: string | null }): EmptyObject;
+}
+
+export interface ExecAsyncApi extends ExecSyncApi {}
+
+export function __internal_execSync<F extends keyof ExecSyncApi>(
+	name: F,
+	params: Parameters<ExecSyncApi[F]>[0],
+): ReturnType<ExecSyncApi[F]> {
+	const resultJson = nativeWhopCore.execSync(name, JSON.stringify(params));
+	if (!resultJson.isOk) {
+		throw new Error(`Failed to execute ${name}: ${resultJson.errorMessage}`);
+	}
+
+	return JSON.parse(resultJson.data || "{}") as ReturnType<ExecSyncApi[F]>;
+}
+
+export async function __internal_execAsync<F extends keyof ExecAsyncApi>(
+	name: F,
+	params: Parameters<ExecAsyncApi[F]>[0],
+): Promise<ReturnType<ExecAsyncApi[F]>> {
+	const resultJson = await nativeWhopCore.execAsync(
+		name,
+		JSON.stringify(params),
+	);
+
+	if (!resultJson.isOk) {
+		throw new Error(`Failed to execute ${name}: ${resultJson.errorMessage}`);
+	}
+
+	return JSON.parse(resultJson.data || "{}") as ReturnType<ExecAsyncApi[F]>;
+}
