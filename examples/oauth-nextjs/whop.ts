468d38962ebe2542711cdc779fb55d47a3d58e3e
Tristan <tristancilltseinleben@gmail.com>
2025-09-04 17:41:50 +0200

    Update examples/oauth-nextjs/whop.ts
    Co-authored-by: vercel[bot] <35613825+vercel[bot]@users.noreply.github.com>

--- examples/oauth-nextjs/whop.ts
+++ examples/oauth-nextjs/whop.ts
@@ -4,7 +4,7 @@ const appId = process.env.NEXT_PUBLIC_WHOP_APP_ID;
 const appApiKey = process.env.WHOP_API_KEY;
 
 if (!appId || !appApiKey) {
-	throw new Error("Missing WHOP_APP_ID or WHOP_API_KEY");
+	throw new Error("Missing NEXT_PUBLIC_WHOP_APP_ID or WHOP_API_KEY");
 }
 
 export const whopSdk = WhopServerSdk({
